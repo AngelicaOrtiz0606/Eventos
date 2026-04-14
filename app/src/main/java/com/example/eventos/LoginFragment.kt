ddf12203333cc6d265d11415de9bab70d86c8fc3
Angélica <angelica.ortiz0606@gmail.com>
2026-04-14 08:52:04 -0600

    Validacion de los campos num cuenta, contraseña y del  botón login

--- app/src/main/java/com/example/eventos/LoginFragment.kt
+++ app/src/main/java/com/example/eventos/LoginFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.widget.addTextChangedListener
 import androidx.navigation.fragment.findNavController
 import com.example.eventos.databinding.FragmentLoginBinding
 
@@ -29,4 +30,52 @@ class LoginFragment : Fragment() {
     }
         return binding.root
     }
+
+    private fun setUpValidation(){
+        binding.loginButton.isEnabled = false
+
+        binding.usernameEdit.addTextChangedListener {
+            validateFields()
+        }
+
+        binding.passwordEdit.addTextChangedListener {
+            validateFields()
+        }
+    }
+
+    private fun validateFields() {
+        val numCuenta = binding.usernameEdit.text.toString().trim()
+        val password = binding.passwordEdit.text.toString().trim()
+
+        val isAccountValid = numCuenta.length == 9
+        val isPasswordValid = password.length >= 8
+
+        // Validación número de cuenta
+        if (numCuenta.isNotEmpty() && !isAccountValid) {
+            binding.usernameLayout.error = "Debe tener 9 dígitos"
+        } else {
+            binding.usernameLayout.error = null
+        }
+
+        // Validación contraseña
+        if (password.isNotEmpty() && !isPasswordValid) {
+            binding.passwordLayout.error = "Mínimo 8 caracteres"
+        } else {
+            binding.passwordLayout.error = null
+        }
+
+        // Activar botón solo si todo es válido
+        binding.loginButton.isEnabled =
+            numCuenta.isNotEmpty() &&
+                    password.isNotEmpty() &&
+                    isAccountValid &&
+                    isPasswordValid
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpValidation()
+    }
+
 }
